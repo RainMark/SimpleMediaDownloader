d4cd5b639a6ac61ebfa8babaac0c70bb5235103d
Rain <rain.by.zhou@gmail.com>
2018-11-10 19:12:03 +0800

    fix container start
    Signed-off-by: Rain <rain.by.zhou@gmail.com>

--- docker/entry.sh
+++ docker/entry.sh
@@ -4,6 +4,8 @@
 
 if [ "2" = "$#" ]; then
     m4 -DSVR_NAME="$2" /config/nginx.conf > /etc/nginx/sites-available/default
+    cd /SimpleMediaDownloader/src/core
+    nohup python3 SimpleMediaDownloader.py -p 10086 &
     service nginx start
     exec /bin/bash
 fi
